94e4c6b1422092830559c945bc97e4ac9e57256e
Sujan <mrsujan321@gmail.com>
2023-04-30 22:35:58 +0545

    my first commit

--- src/App.js
+++ src/App.js
@@ -15,9 +15,9 @@ function App(){
     <Router>
       <Header/>
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/contact' element={<Contact/>} />
+        <Route exact path='/' element={<Home/>} />
+        <Route exact path='/about' element={<About/>} />
+        <Route exact path='/contact' element={<Contact/>} />
       </Routes>
       <Footer/>
     </Router>
